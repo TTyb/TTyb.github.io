d4110c8309b5b467efaf7ed9de62940b018cd69c
TTyb <420439007@qq.com>
2017-01-09 17:25:08 +0800

    cloud

---
+++ static/js/cloudjs.js
@@ -0,0 +1,6 @@
+/*在线演示公共底部*/
+$(function(){
+	/*头部导航*/
+	$('body').append('<div class="top-banner clearfix"><div class="fl"><a href="http://www.yyyweb.com/377.html" target="_blank">&lt;&lt;8个前沿的 HTML5 & CSS3 效果【附源码下载】</a></div><div class="fr"><a href="http://www.yyyweb.com/350.html" target="_blank">Web 开发中很实用的10个效果【附源码下载】&gt;&gt;</a></div></div>');
+	// $('body').append('<div class="top-banner clearfix"><div class="fl"><a href="http://www.cnblogs.com/lhb25/p/10-useful-html5-css3-effect.html" target="_blank">&lt;&lt;8个前沿的 HTML5 & CSS3 效果【附源码下载】</a></div><div class="fr"><a href="http://www.cnblogs.com/lhb25/p/10-useful-web-effect.html" target="_blank">Web 开发中很实用的10个效果【附源码下载】&gt;&gt;</a></div></div>');
+});
