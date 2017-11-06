121f1eb9d42364d9e02ff36ef89fb78f5d1cbefd
TTyb <420439007@qq.com>
2017-11-06 22:24:43 +0800

    tbtmGoods

--- code/tbtmGoods.py
+++ code/tbtmGoods.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import json
+import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5"
@@ -11,7 +13,7 @@ session = requests.session()
 session.get("https://s.m.taobao.com/", headers=headers)
 
 
-def getJson():
+def getJson(page, keyword):
     getData = {
         "event_submit_do_new_search_auction": "1",
         "search": "提交",
@@ -22,7 +24,7 @@ def getJson():
         "searchfrom": "1",
         "action": "home:redirect_app_action",
         "from": "1",
-        "q": "iphonex",
+        "q": keyword,
         "sst": "1",
         "n": "20",
         "buying": "buyitnow",
@@ -31,7 +33,7 @@ def getJson():
         "wlsort": "30",
         "style": "list",
         "closeModues": "nav,selecthot,onesearch",
-        "page": "2"
+        "page": page
     }
 
     preUrl = "http://s.m.taobao.com/search?"
@@ -44,12 +46,56 @@ def getJson():
     return aliUrl.content
 
 
+# 解析抓取到的json
+def bytesToDict(content):
+    dictInfo = json.loads(content.decode("utf-8", "ignore"))
+    infoList = dictInfo["listItem"]
+    return infoList
+
+
+# 格式化字典
+def formatDict(page, infoList):
+    dictList = []
+    for listItem in infoList:
+        formatInfo = {}
+        formatInfo["页数"] = page
+        Trys("店名","nick",formatInfo,listItem)
+        Trys("商品标题","title",formatInfo,listItem)
+        Trys("商品打折价","price",formatInfo,listItem)
+        Trys("发货地址","location",formatInfo,listItem)
+        Trys("评论数","commentCount",formatInfo,listItem)
+        Trys("原价","originalPrice",formatInfo,listItem)
+        Trys("售出件数","act",formatInfo,listItem)
+        Trys("折扣政策","zkType",formatInfo,listItem)
+        Trys("付款人数","act",formatInfo,listItem)
+        Trys("金币折扣","coinLimit",formatInfo,listItem)
+        Trys("详情页","url",formatInfo,listItem)
+        Trys("图像URL","pic_path",formatInfo,listItem)
+        # 修饰value
+        formatInfo["详情页"] = "https:" + formatInfo["详情页"]
+        formatInfo["详情页"] = formatInfo["详情页"].replace('60x60', '720x720')
+        dictList.append(formatInfo)
+
+    return dictList
+
+# 一个try
+def Trys(key1,key2,dict1,dict2):
+    try:
+        dict1[key1] = dict2[key2]
+    except:
+        dict1[key1] = "None"
+    return dict1
+
 def main():
-    jsonInfo = getJson()
-    file = open("html.json", 'wb')
-    file.write(jsonInfo)
-    file.close()
-    print(jsonInfo)
+    page = 2
+    keyword = "iphone x".replace(" ", "+")
+
+    for item in range(0, page):
+        jsonInfo = getJson(page + 1, keyword)
+        infoList = bytesToDict(jsonInfo)
+        dictList = formatDict(page, infoList)
+        print(dictList)
+        time.sleep(5)
 
 
 if __name__ == "__main__":
